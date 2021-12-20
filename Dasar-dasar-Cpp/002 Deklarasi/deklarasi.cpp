5707f4ba2937e3c4046c3335ee4fbfcab039b57a
ifandika <ifandika014@gmail.com>
2021-12-20 23:17:47 +0700

    Menambahkan Folder dan File 002 Deklarasi/deklarasi.cpp

---
+++ Dasar-dasar-Cpp/002 Deklarasi/deklarasi.cpp
@@ -0,0 +1,20 @@
+// #include -> Meinclude/mengcopy semua file 
+// <iostream> -> File yang digunakan untuk memprogram C++/cpp
+#include <iostream>
+
+// Baris kosong tidak di exsekusi
+// int main(){
+//               -> Body program, semua yang ada di dalam akan di exsekusi
+//}
+int main(){
+  
+  // std:: -> standard libary untuk cpp
+  // <<    -> simbol untuk output
+  // >>    -> simbol untuk input
+  // cout  -> console out, menampilkan data ke console/terminal/cmd
+  // endl  -> untuk membuat new line, baris baru di bawahnya
+  // ( ; ) -> program di akhiri dengan titik koma
+  std::cout << "Deklarasi pada C++/cpp" << std::endl;
+  // Return 0 -> Mengembalikan nilai 0 ke main
+  return 0;
+}
